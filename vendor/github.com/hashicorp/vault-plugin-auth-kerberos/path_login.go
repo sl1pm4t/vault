b00034c3aec1b32ef360ddc66ef6f20a61e21f89
Becca Petrin <beccapetrin@gmail.com>
2019-08-07 15:43:07 -0700

    get most recent provider

--- vendor/github.com/hashicorp/vault-plugin-auth-kerberos/path_login.go
+++ vendor/github.com/hashicorp/vault-plugin-auth-kerberos/path_login.go
@@ -8,28 +8,32 @@ import (
 	"strings"
 
 	"github.com/hashicorp/vault/sdk/framework"
+	"github.com/hashicorp/vault/sdk/helper/cidrutil"
 	"github.com/hashicorp/vault/sdk/helper/ldaputil"
 	"github.com/hashicorp/vault/sdk/helper/strutil"
 	"github.com/hashicorp/vault/sdk/logical"
-
 	"gopkg.in/jcmturner/gokrb5.v5/credentials"
 	"gopkg.in/jcmturner/gokrb5.v5/gssapi"
 	"gopkg.in/jcmturner/gokrb5.v5/keytab"
 	"gopkg.in/jcmturner/gokrb5.v5/service"
 )
 
-func pathLogin(b *backend) *framework.Path {
+func (b *backend) pathLogin() *framework.Path {
 	return &framework.Path{
 		Pattern: "login$",
 		Fields: map[string]*framework.FieldSchema{
-			"authorization": &framework.FieldSchema{
+			"authorization": {
 				Type:        framework.TypeString,
 				Description: `SPNEGO Authorization header. Required.`,
 			},
 		},
-		Callbacks: map[logical.Operation]framework.OperationFunc{
-			logical.ReadOperation:   b.pathLoginGet,
-			logical.UpdateOperation: b.pathLogin,
+		Operations: map[logical.Operation]framework.OperationHandler{
+			logical.ReadOperation: &framework.PathOperation{
+				Callback: b.pathLoginGet,
+			},
+			logical.UpdateOperation: &framework.PathOperation{
+				Callback: b.pathLoginUpdate,
+			},
 		},
 	}
 }
@@ -46,27 +50,33 @@ func parseKeytab(stringKeytab string) (*keytab.Keytab, error) {
 	return &kt, nil
 }
 
-func spnegoKrb5Authenticate(kt keytab.Keytab, sa string, authorization []byte, remoteAddr string) (bool, *credentials.Credentials, error) {
+func spnegoKrb5Authenticate(kt keytab.Keytab, sa string, authorization []byte, remoteAddr string) (*credentials.Credentials, error) {
 	var spnego gssapi.SPNEGO
-	err := spnego.Unmarshal(authorization)
-	if err != nil || !spnego.Init {
-		return false, nil, fmt.Errorf("SPNEGO negotiation token is not a NegTokenInit: %v", err)
+	if err := spnego.Unmarshal(authorization); err != nil || !spnego.Init {
+		return nil, fmt.Errorf("SPNEGO negotiation token is not a NegTokenInit: %v", err)
 	}
 	if !spnego.NegTokenInit.MechTypes[0].Equal(gssapi.MechTypeOIDKRB5) && !spnego.NegTokenInit.MechTypes[0].Equal(gssapi.MechTypeOIDMSLegacyKRB5) {
-		return false, nil, errors.New("SPNEGO OID of MechToken is not of type KRB5")
+		return nil, errors.New("SPNEGO OID of MechToken is not of type KRB5")
 	}
 
 	var mt gssapi.MechToken
-	err = mt.Unmarshal(spnego.NegTokenInit.MechToken)
-	if err != nil {
-		return false, nil, fmt.Errorf("SPNEGO error unmarshaling MechToken: %v", err)
+	if err := mt.Unmarshal(spnego.NegTokenInit.MechToken); err != nil {
+		return nil, fmt.Errorf("SPNEGO error unmarshaling MechToken: %v", err)
 	}
 	if !mt.IsAPReq() {
-		return false, nil, errors.New("MechToken does not contain an AP_REQ - KRB_AP_ERR_MSG_TYPE")
+		return nil, errors.New("MechToken does not contain an AP_REQ - KRB_AP_ERR_MSG_TYPE")
 	}
 
-	ok, creds, err := service.ValidateAPREQ(mt.APReq, kt, sa, remoteAddr, false)
-	return ok, &creds, err
+	// The first return value here is a boolean reflecting whether the request is valid;
+	// however, this value is redundant because if the error is nil, the request is valid,
+	// but if it's populated, the request is invalid. Hence, it's ignored here because we
+	// only need to error if the error is populated, and that knowledge can be encapsulated
+	// here.
+	_, creds, err := service.ValidateAPREQ(mt.APReq, kt, sa, remoteAddr, false)
+	if err != nil {
+		return nil, err
+	}
+	return &creds, nil
 }
 
 func (b *backend) pathLoginGet(ctx context.Context, req *logical.Request, d *framework.FieldData) (*logical.Response, error) {
@@ -78,26 +88,37 @@ func (b *backend) pathLoginGet(ctx context.Context, req *logical.Request, d *fra
 	}, logical.CodedError(401, "authentication required")
 }
 
-func (b *backend) pathLogin(ctx context.Context, req *logical.Request, d *framework.FieldData) (*logical.Response, error) {
-	config, err := b.config(ctx, req.Storage)
+func (b *backend) pathLoginUpdate(ctx context.Context, req *logical.Request, d *framework.FieldData) (*logical.Response, error) {
+	kerbCfg, err := b.config(ctx, req.Storage)
 	if err != nil {
 		return nil, err
 	}
-	if config == nil {
-		return nil, errors.New("Could not load backend configuration")
+	if kerbCfg == nil {
+		return nil, errors.New("backend kerberos not configured")
 	}
 
-	kt, err := parseKeytab(config.Keytab)
-	if err != nil {
-		return nil, fmt.Errorf("Could not load keytab: %v", err)
-	}
-
-	ldapConfig, err := b.ConfigLdap(ctx, req)
+	kt, err := parseKeytab(kerbCfg.Keytab)
 	if err != nil {
 		return nil, err
 	}
-	if ldapConfig == nil {
+
+	ldapCfg, err := b.ConfigLdap(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	if ldapCfg == nil {
 		return nil, errors.New("ldap backend not configured")
+	}
+
+	// Check for a CIDR match.
+	if len(ldapCfg.TokenBoundCIDRs) > 0 {
+		if req.Connection == nil {
+			b.Logger().Warn("token bound CIDRs found but no connection information available for validation")
+			return nil, logical.ErrPermissionDenied
+		}
+		if !cidrutil.RemoteAddrIsOk(req.Connection.RemoteAddr, ldapCfg.TokenBoundCIDRs) {
+			return nil, logical.ErrPermissionDenied
+		}
 	}
 
 	ldapClient := ldaputil.Client{
@@ -105,9 +126,9 @@ func (b *backend) pathLogin(ctx context.Context, req *logical.Request, d *framew
 		LDAP:   ldaputil.NewLDAP(),
 	}
 
-	ldapConnection, err := ldapClient.DialLDAP(ldapConfig)
+	ldapConnection, err := ldapClient.DialLDAP(ldapCfg.ConfigEntry)
 	if err != nil {
-		return nil, fmt.Errorf("Could not connect to LDAP: %v", err)
+		return nil, fmt.Errorf("could not connect to LDAP: %v", err)
 	}
 	if ldapConnection == nil {
 		return nil, errors.New("invalid connection returned from LDAP dial")
@@ -130,48 +151,39 @@ func (b *backend) pathLogin(ctx context.Context, req *logical.Request, d *framew
 	}
 	authorization, err := base64.StdEncoding.DecodeString(s[1])
 	if err != nil {
-		return nil, fmt.Errorf("Could not base64 decode authorization: %v", err)
+		return nil, fmt.Errorf("could not base64 decode authorization: %v", err)
 	}
 
-	ok, creds, err := spnegoKrb5Authenticate(*kt, config.ServiceAccount, authorization, req.Connection.RemoteAddr)
-	if !ok {
-		if err != nil {
-			return nil, err
-		} else {
-			return logical.ErrorResponse("Kerberos authentication failed"), nil
-		}
+	creds, err := spnegoKrb5Authenticate(*kt, kerbCfg.ServiceAccount, authorization, req.Connection.RemoteAddr)
+	if err != nil {
+		return nil, err
 	}
 
-	if len(ldapConfig.BindPassword) > 0 {
-		err = ldapConnection.Bind(ldapConfig.BindDN, ldapConfig.BindPassword)
+	if len(ldapCfg.BindPassword) > 0 {
+		err = ldapConnection.Bind(ldapCfg.BindDN, ldapCfg.BindPassword)
 	} else {
-		err = ldapConnection.UnauthenticatedBind(ldapConfig.BindDN)
+		err = ldapConnection.UnauthenticatedBind(ldapCfg.BindDN)
 	}
 	if err != nil {
 		return nil, fmt.Errorf("LDAP bind failed: %v", err)
 	}
 
-	userBindDN, err := ldapClient.GetUserBindDN(ldapConfig, ldapConnection, creds.Username)
+	userBindDN, err := ldapClient.GetUserBindDN(ldapCfg.ConfigEntry, ldapConnection, creds.Username)
+	if err != nil {
+		return nil, err
+	}
+	b.Logger().Debug("auth/ldap: User BindDN fetched", "username", creds.Username, "binddn", userBindDN)
+
+	userDN, err := ldapClient.GetUserDN(ldapCfg.ConfigEntry, ldapConnection, userBindDN)
 	if err != nil {
 		return nil, err
 	}
 
-	if b.Logger().IsDebug() {
-		b.Logger().Debug("auth/ldap: User BindDN fetched", "username", creds.Username, "binddn", userBindDN)
-	}
-
-	userDN, err := ldapClient.GetUserDN(ldapConfig, ldapConnection, userBindDN)
+	ldapGroups, err := ldapClient.GetLdapGroups(ldapCfg.ConfigEntry, ldapConnection, userDN, creds.Username)
 	if err != nil {
 		return nil, err
 	}
-
-	ldapGroups, err := ldapClient.GetLdapGroups(ldapConfig, ldapConnection, userDN, creds.Username)
-	if err != nil {
-		return nil, err
-	}
-	if b.Logger().IsDebug() {
-		b.Logger().Debug("auth/ldap: Groups fetched from server", "num_server_groups", len(ldapGroups), "server_groups", ldapGroups)
-	}
+	b.Logger().Debug("auth/ldap: Groups fetched from server", "num_server_groups", len(ldapGroups), "server_groups", ldapGroups)
 
 	var allGroups []string
 	// Merge local and LDAP groups
@@ -181,26 +193,44 @@ func (b *backend) pathLogin(ctx context.Context, req *logical.Request, d *framew
 	var policies []string
 	for _, groupName := range allGroups {
 		group, err := b.Group(ctx, req.Storage, groupName)
-		if err == nil && group != nil {
-			policies = append(policies, group.Policies...)
+		if err != nil {
+			b.Logger().Warn(fmt.Sprintf("unable to retrieve %s: %s", groupName, err.Error()))
+			continue
 		}
+		if group == nil {
+			b.Logger().Warn(fmt.Sprintf("unable to find %s, does not currently exist", groupName))
+			continue
+		}
+		policies = append(policies, group.Policies...)
 	}
 	// Policies from each group may overlap
 	policies = strutil.RemoveDuplicates(policies, true)
 
-	return &logical.Response{
-		Auth: &logical.Auth{
-			InternalData: map[string]interface{}{},
-			Policies:     policies,
-			Metadata: map[string]string{
-				"user":  creds.Username,
-				"realm": creds.Realm,
-			},
-			DisplayName: creds.Username,
-			Alias:       &logical.Alias{Name: creds.Username},
-			LeaseOptions: logical.LeaseOptions{
-				Renewable: false,
-			},
+	auth := &logical.Auth{
+		InternalData: map[string]interface{}{},
+		Metadata: map[string]string{
+			"user":  creds.Username,
+			"realm": creds.Realm,
 		},
+		DisplayName: creds.Username,
+		Alias:       &logical.Alias{Name: creds.Username},
+	}
+
+	ldapCfg.PopulateTokenAuth(auth)
+
+	// This is done after PopulateTokenAuth because it forces Renewable to be true.
+	// Renewable was always false at the time of the code's introduction, and we would
+	// like to keep it the same until we have a concrete reason to change its behavior.
+	auth.LeaseOptions = logical.LeaseOptions{
+		Renewable: false,
+	}
+
+	// Combine our policies with the ones parsed from PopulateTokenAuth.
+	if len(policies) > 0 {
+		auth.Policies = append(auth.Policies, policies...)
+	}
+
+	return &logical.Response{
+		Auth: auth,
 	}, nil
 }
